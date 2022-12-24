27e6c2e6534ddbd5e88316b8f80f7a8eceefee03
heraya <herayafpm@gmail.com>
2022-12-24 10:45:35 +0700

    finish adminlte

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,187 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{ $page_title ?? 'Dashboard' }} - Resto</title>
+
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+
+    <link rel="stylesheet" href="{{ asset('assets/vendor/adminlte') }}/plugins/fontawesome-free/css/all.min.css">
+
+    <link rel="stylesheet" href="{{ asset('assets/vendor/adminlte') }}/dist/css/adminlte.min.css?v=3.2.0">
+    <script nonce="b4280936-d779-40bf-b82a-d0f55cb84e4e">
+        (function(w, d) {
+            ! function(eK, eL, eM, eN) {
+                eK.zarazData = eK.zarazData || {};
+                eK.zarazData.executed = [];
+                eK.zaraz = {
+                    deferred: [],
+                    listeners: []
+                };
+                eK.zaraz.q = [];
+                eK.zaraz._f = function(eO) {
+                    return function() {
+                        var eP = Array.prototype.slice.call(arguments);
+                        eK.zaraz.q.push({
+                            m: eO,
+                            a: eP
+                        })
+                    }
+                };
+                for (const eQ of ["track", "set", "debug"]) eK.zaraz[eQ] = eK.zaraz._f(eQ);
+                eK.zaraz.init = () => {
+                    var eR = eL.getElementsByTagName(eN)[0],
+                        eS = eL.createElement(eN),
+                        eT = eL.getElementsByTagName("title")[0];
+                    eT && (eK.zarazData.t = eL.getElementsByTagName("title")[0].text);
+                    eK.zarazData.x = Math.random();
+                    eK.zarazData.w = eK.screen.width;
+                    eK.zarazData.h = eK.screen.height;
+                    eK.zarazData.j = eK.innerHeight;
+                    eK.zarazData.e = eK.innerWidth;
+                    eK.zarazData.l = eK.location.href;
+                    eK.zarazData.r = eL.referrer;
+                    eK.zarazData.k = eK.screen.colorDepth;
+                    eK.zarazData.n = eL.characterSet;
+                    eK.zarazData.o = (new Date).getTimezoneOffset();
+                    if (eK.dataLayer)
+                        for (const eX of Object.entries(Object.entries(dataLayer).reduce(((eY, eZ) => ({
+                                ...eY[1],
+                                ...eZ[1]
+                            }))))) zaraz.set(eX[0], eX[1], {
+                            scope: "page"
+                        });
+                    eK.zarazData.q = [];
+                    for (; eK.zaraz.q.length;) {
+                        const e_ = eK.zaraz.q.shift();
+                        eK.zarazData.q.push(e_)
+                    }
+                    eS.defer = !0;
+                    for (const fa of [localStorage, sessionStorage]) Object.keys(fa || {}).filter((fc => fc
+                        .startsWith("_zaraz_"))).forEach((fb => {
+                        try {
+                            eK.zarazData["z_" + fb.slice(7)] = JSON.parse(fa.getItem(fb))
+                        } catch {
+                            eK.zarazData["z_" + fb.slice(7)] = fa.getItem(fb)
+                        }
+                    }));
+                    eS.referrerPolicy = "origin";
+                    eS.src = "/cdn-cgi/zaraz/s.js?z=" + btoa(encodeURIComponent(JSON.stringify(eK.zarazData)));
+                    eR.parentNode.insertBefore(eS, eR)
+                };
+                ["complete", "interactive"].includes(eL.readyState) ? zaraz.init() : eK.addEventListener(
+                    "DOMContentLoaded", zaraz.init)
+            }(w, d, 0, "script");
+        })(window, document);
+    </script>
+    @yield('css')
+</head>
+
+<body class="hold-transition sidebar-mini">
+    <div class="wrapper">
+        <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i
+                            class="fas fa-bars"></i></a>
+                </li>
+            </ul>
+        </nav>
+        <aside class="main-sidebar sidebar-dark-primary elevation-4">
+            <a href="../../index3.html" class="brand-link">
+                <img src="{{ asset('assets/vendor/adminlte') }}/dist/img/AdminLTELogo.png" alt="AdminLTE Logo"
+                    class="brand-image img-circle elevation-3" style="opacity: .8">
+                <span class="brand-text font-weight-light">Resto</span>
+            </a>
+            <div class="sidebar">
+                <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+                    <div class="image">
+                        <img src="{{ asset('assets/vendor/adminlte') }}/dist/img/user2-160x160.jpg"
+                            class="img-circle elevation-2" alt="User Image">
+                    </div>
+                    <div class="info">
+                        <a href="#" class="d-block">{{ Auth::user()->name }}</a>
+                    </div>
+                </div>
+                <nav class="mt-2">
+                    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
+                        data-accordion="false">
+                        <li class="nav-item">
+                            <a href="{{route('admin.dashboard')}}" class="nav-link {{request()->is('admin/dashboard')?'active':''}}">
+                                <i class="nav-icon fas fa-tachometer-alt"></i>
+                                <p>
+                                    Dashboard
+                                </p>
+                            </a>
+                        </li>
+                        <li class="nav-item {{request()->is('admin/master/*')?'menu-open':''}}">
+                            <a href="#" class="nav-link">
+                                <i class="nav-icon fas fa-file"></i>
+                                <p>
+                                    Master
+                                    <i class="right fas fa-angle-left"></i>
+                                </p>
+                            </a>
+                            <ul class="nav nav-treeview">
+                                <li class="nav-item">
+                                    <a href="{{route('admin.master.daftar_makanan')}}" class="nav-link {{request()->is('admin/master/daftar_makanan/*')?'active':''}}">
+                                        <i class="far fa-circle nav-icon"></i>
+                                        <p>Daftar Makanan</p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="{{route('admin.master.resto')}}" class="nav-link {{request()->is('admin/master/resto/*')?'active':''}}">
+                                        <i class="far fa-circle nav-icon"></i>
+                                        <p>Resto</p>
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+                                <a href="{{ route('logout') }}" class="nav-link bg-danger" onclick="event.preventDefault();
+                                this.closest('form').submit();">
+                                    <i class="nav-icon fas fa-power-off"></i>
+                                    <p>
+                                        Logout
+                                    </p>
+                                </a>
+                            </form>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </aside>
+        <div class="content-wrapper">
+            <section class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-12">
+                            <h1>{{ $page_title ?? 'Dashboard' }}</h1>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section class="content">
+                @yield('content')
+            </section>
+        </div>
+        <footer class="main-footer">
+            <div class="float-right d-none d-sm-block">
+                <b>Version</b> 3.2.0
+            </div>
+            <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights
+            reserved.
+        </footer>
+    </div>
+    <script src="{{ asset('assets/vendor/adminlte') }}/plugins/jquery/jquery.min.js"></script>
+    <script src="{{ asset('assets/vendor/adminlte') }}/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ asset('assets/vendor/adminlte') }}/dist/js/adminlte.min.js?v=3.2.0"></script>
+    @yield('js')
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,5 +21,8 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+Route::group(['prefix' => 'admin','as' => 'admin.','namespace'=> 'Admin','middleware' => ['auth']],function(){
+    Route::get('dashboard',[DashboardController::class,'index'])->name('dashboard');
+}); 
 
 require __DIR__.'/auth.php';
